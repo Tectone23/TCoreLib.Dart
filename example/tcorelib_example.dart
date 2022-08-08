6c58da3e6c85b205db1cb78ddf0de6553bca361a
HUSKI3 <ignispy@protonmail.com>
2022-08-08 15:18:42 +0200

    upd: Add new usage to the example

--- example/tcorelib_example.dart
+++ example/tcorelib_example.dart
@@ -1,5 +1,30 @@
+import 'package:tcorelib/tcorelib.dart';
+import 'dart:convert';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+Future<void> main() async {
+  /// Create a client for TCore, 
+  /// it'll populate itself with an empty Asgard object
+  var client = Client();
+
+  /// Update Asgard's contents, with the reponse from the server
+  /// ie - Modules and etc
+  await client.InitRest();
+
+  /// Output each module seperately 
+  for (final module in client.asgard.modules) {
+      print(module);
+  }
+
+  /// Let's try to send a request to a sample ethereum cog
+  var response = await client.sendRequest(
+    {
+      "hook": "ethereum",
+      "action": "ethereum_balance",
+      "function": "getBalance",
+      "params": "0x00000000219ab540356cBB839Cbe05303d7705Fa"
+    }
+  );
+  /// Decode response
+  var data = jsonDecode(response.body);
+  print(data);
 }
